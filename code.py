c628d5ecc0032b66581c74b67f58bb9f2f7341f4
cashhollister2u <153677541+cashhollister2u@users.noreply.github.com>
2024-05-06 18:05:28 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,11 @@
 # adafruit
-import displayio
 from adafruit_matrixportal.matrix import Matrix
 
 # conventional libraries
 from os import getenv
 import time
-import gc
 
 # custom components
-from views.view_stock import display_stock
-from views.view_clock import display_clock
-from components.clock import increment_time, JSON_clock 
 from components.Spotipy_circuitpython.Authentication import refresh_access_token
 from components.Spotipy_circuitpython.Player.player import spotify_player
 from components.Spotipy_circuitpython.Player.player_parameters import currently_playing, pause_playback, resume_playback, set_playback_volume, skip_to_next, skip_to_previous
@@ -20,12 +15,13 @@ IS_SPOTIFY = getenv("IS_SPOTIFY")
 GLOBAL_ACCESS_TOKEN = refresh_access_token() # spotify
 
 
-# if output i will print in the terminal
-if IS_SPOTIFY == 1:
+# if output then it will print in the terminal
+while IS_SPOTIFY == 1: 
+#if IS_SPOTIFY == 1:
     print('spotify active')
-    #spotify_player(currently_playing, GLOBAL_ACCESS_TOKEN)
-    #if not GLOBAL_ACCESS_TOKEN:
-    #    break
+    spotify_player(currently_playing, GLOBAL_ACCESS_TOKEN)
+    if not GLOBAL_ACCESS_TOKEN:
+        break
 
     #spotify_player(pause_playback, GLOBAL_ACCESS_TOKEN)
 
@@ -35,6 +31,7 @@ if IS_SPOTIFY == 1:
     
     #spotify_player(skip_to_next, GLOBAL_ACCESS_TOKEN)
 
-    spotify_player(skip_to_previous, GLOBAL_ACCESS_TOKEN)
+    #spotify_player(skip_to_previous, GLOBAL_ACCESS_TOKEN)
 
-    time.sleep(5)
+    time.sleep(5) #FOR THE WHILE LOOP OPTION
+
